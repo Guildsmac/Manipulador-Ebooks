44926f2c9df668f97d77e5c276d3a55733604153
Gabriel Sousa <gabrielssc.ti@gmail.com>
2018-10-07 23:30:37 -0300

    Fixed folder Manipulator

--- FolderManipulator.php
+++ FolderManipulator.php
@@ -20,10 +20,10 @@ class FolderManipulator{
     }
     public static function getNewFolder($path, $folderName){
         $cont = 1;
-        $actFolder = $path . "\\" . $folderName . "#" . $cont;
+        $actFolder = $path . "\\" . $folderName . "-" . $cont;
         if(file_exists($actFolder)){
             while(file_exists($actFolder))
-                $actFolder = $path . "\\" . $folderName . "#" . ++$cont;
+                $actFolder = $path . "\\" . $folderName . "-" . ++$cont;
         }
         mkdir($actFolder, 0777);
         return $actFolder;
