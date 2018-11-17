504c958d909c1605e33c8c0e5c05e6983fb32384
Gabriel Sousa <gabrielssc.ti@gmail.com>
2018-11-17 01:45:09 -0300

    Added paths normalizer

--- NameManipulator.php
+++ NameManipulator.php
@@ -24,4 +24,13 @@ class NameManipulator{
         return $path;
     }
 
+    public static function normalizePath($path){
+        $path = str_replace( '\\', '/', $path );
+        $path = preg_replace( '|(?<=.)/+|', '/', $path );
+        if ( ':' === substr( $path, 1, 1 ) ) {
+            $path = ucfirst( $path );
+        }
+        return $path;
+    }
+
 }
